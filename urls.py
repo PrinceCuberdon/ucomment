857c1d0c80e2f96b946840e8a7c18cb2b5a53bbf
princecuberdon <princecuberdon@bandcochon.fr>
2016-06-30 12:04:19 +0200

    Remove all Python warnings when starting with `python -Wall` option

--- urls.py
+++ urls.py
@@ -2,24 +2,24 @@
 # ucomment is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
 #
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from .views import BookView
+import ucomment.views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(
         r'^book/$',
-        BookView.as_view(),
+        ucomment.views.BookView.as_view(),
         name="ucomment_book"
     ),
 
-    url(r'^book/next/$',             'ucomment.views.book_next'),
+    url(r'^book/next/$',             ucomment.views.book_next),
 
-    url(r'^ucomment/postmessage/$',  'ucomment.views.postmessage',    name='ucomment_postmessage'),
-    url(r'^ucomment/agree/$',        'ucomment.views.agree',          name="ucomment_agree"),
-    url(r'^ucomment/disagree/$',     'ucomment.views.disagree',       name='ucomment_disagree'),
-    url(r'^ucomment/moderate/$',     'ucomment.views.moderate',       name='ucomment_moderate'),
-    url(r'^ucomment/nextcomment/$',  'ucomment.views.nextcomment',    name='ucomment_nextcomment'),
-    url(r'^ucomment/showlast/$',     'ucomment.views.showlast',       name='ucomment_showlast'),
-    url(r'^ucomment/sendphoto/$',    'ucomment.views.sendphoto',      name="ucomment_sendphoto"),
-)
+    url(r'^ucomment/postmessage/$',  ucomment.views.postmessage,    name='ucomment_postmessage'),
+    url(r'^ucomment/agree/$',        ucomment.views.agree,          name="ucomment_agree"),
+    url(r'^ucomment/disagree/$',     ucomment.views.disagree,       name='ucomment_disagree'),
+    url(r'^ucomment/moderate/$',     ucomment.views.moderate,       name='ucomment_moderate'),
+    url(r'^ucomment/nextcomment/$',  ucomment.views.nextcomment,    name='ucomment_nextcomment'),
+    url(r'^ucomment/showlast/$',     ucomment.views.showlast,       name='ucomment_showlast'),
+    url(r'^ucomment/sendphoto/$',    ucomment.views.sendphoto,      name="ucomment_sendphoto"),
+]
